0c526a38cbc3fee0253b9774c537469e7aa850c9
Benjamin Daschel <coffeemaxed@gmail.com>
2015-04-03 00:15:21 -0700

    notes and formatting

--- src/main/java/Homework1.java
+++ src/main/java/Homework1.java
@@ -8,6 +8,7 @@ import java.util.List;
 public class Homework1 {
 
     public static void main(String[] args) {
+
         // Gotta love them varargs methods
         List<GameCharacter> characters = Arrays.asList(
                 new AngusYoung(),
@@ -15,12 +16,28 @@ public class Homework1 {
                 new JimiHendrix()
         );
 
+        /*
+            Demonstrates the behavior of each character when no Solos or Guitars
+            have been set on the instances.  See method documentation for more details.
+         */
         for(GameCharacter gc : characters){
             gc.sayName();
             gc.playGuitar();
             gc.performSolo();
         }
 
+        for(GameCharacter gc : characters){
+
+            /*
+                And now boys and girls, that's what a fluent interface looks like.
+                In the case of some more complex classes where many options can be specified,
+                a Builder class is used instead. The Builder class will usually contain all the setters
+                with the validation logic and state machine management and simply output a ready to use object.
+             */
+            gc.setGuitar(new FenderTelecaster())
+                    .setSolo(new GuitarOnFire());
+        }
 
     }
+
 }
