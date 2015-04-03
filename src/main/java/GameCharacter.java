db782e910fa801a0c3c8ec1e3f9076034f98df23
Benjamin Daschel <coffeemaxed@gmail.com>
2015-04-03 05:40:07 -0700

    comments

--- src/main/java/GameCharacter.java
+++ src/main/java/GameCharacter.java
@@ -55,6 +55,18 @@ public abstract class GameCharacter {
         System.out.println(this.getName());
     }
 
+    /**
+     * Why not just implement this method right here you ask?  Well,
+     * that would require me to use a member variable to hold the name. That's
+     * not a big deal, except that then I would have to call a setter from within the
+     * subclass's constructor or implement a super constructor with arguments.  Please
+     * see the README.md included with this project for more details.
+     *
+     * Also notice the fact that there is no setName method. This is intended;
+     * there need not be an interface to swap the character's name at runtime.
+     *
+     * @return The player's name
+     */
     protected abstract String getName();
 
 }
